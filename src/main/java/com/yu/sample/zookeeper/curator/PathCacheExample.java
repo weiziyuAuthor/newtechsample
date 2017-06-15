787b8bd4d3477d6043818769520ccc0ebe790bbf
ziyu.wei <522746956@qq.com>
2017-06-15 16:59:20 +0800

    curator cache

--- src/main/java/com/yu/sample/zookeeper/curator/PathCacheExample.java
+++ src/main/java/com/yu/sample/zookeeper/curator/PathCacheExample.java
@@ -1,5 +1,14 @@
 package com.yu.sample.zookeeper.curator;
 
+import org.apache.curator.framework.CuratorFramework;
+import org.apache.curator.framework.CuratorFrameworkFactory;
+import org.apache.curator.framework.recipes.cache.PathChildrenCache;
+import org.apache.curator.framework.recipes.cache.PathChildrenCacheEvent;
+import org.apache.curator.framework.recipes.cache.PathChildrenCacheListener;
+import org.apache.curator.retry.ExponentialBackoffRetry;
+import org.apache.curator.utils.CloseableUtils;
+import org.apache.curator.utils.ZKPaths;
+
 /**
  * 
  * @author weizy
@@ -12,8 +21,49 @@ public class PathCacheExample {
 
 	private static final String PATH = "/example/cache";
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 //		TestingServer server = new TestingServer();
+		CuratorFramework client = null;
+		PathChildrenCache cache = null;
+		
+		try{
+			client = CuratorFrameworkFactory.newClient("", new ExponentialBackoffRetry(1000, 3));
+			client.start();
+			
+			cache = new PathChildrenCache(client, PATH, true);
+			cache.start();
+			
+			processCommands(client, cache);
+			
+		}finally{
+			CloseableUtils.closeQuietly(cache);
+			CloseableUtils.closeQuietly(client);
+		}
+	}
+	
+	private static void addListener(PathChildrenCache cache) {
+		PathChildrenCacheListener listener = new PathChildrenCacheListener(){
+
+			public void childEvent(CuratorFramework arg0,
+					PathChildrenCacheEvent event) throws Exception {
+				switch(event.getType()){
+				case CHILD_ADDED:
+					System.out.println("Node added: " + ZKPaths.getNodeFromPath(event.getData().getPath()));
+					break;
+				case CHILD_UPDATED:
+					System.out.println("Node changed: " + ZKPaths.getNodeFromPath(event.getData().getPath()));
+					break;
+				case CHILD_REMOVED:
+					System.out.println("Node removed: " + ZKPaths.getNodeFromPath(event.getData().getPath()));
+					break;
+				}
+			}
+			
+		};
+		cache.getListenable().addListener(listener);
+	}
+	
+	private static void processCommands(CuratorFramework client, PathChildrenCache cache) {
 		
 	}
 }
